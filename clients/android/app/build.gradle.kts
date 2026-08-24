63a9dbf59f040a30628e827dc2217d619b294be1
mikutea <61963176+mikutea@users.noreply.github.com>
2026-08-24 21:19:42 +0800

    chore(android): prepare 0.3.0-beta.2

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.server.smsforwarder"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0-beta.1"
+        versionCode = 4
+        versionName = "0.3.0-beta.2"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
